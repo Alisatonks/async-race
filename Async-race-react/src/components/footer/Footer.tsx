9e32b8c391b39e67506b554a66f08ebe5718c99c
Volha Haiduk <Alisatonks2385@gmail.com>
2024-05-03 15:55:36 +0200

    feat: add footer

---
+++ Async-race-react/src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <a
+          href="https://github.com/Alisatonks"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h5>Volha Haiduk</h5>
+        </a>
+        <p>2024</p>
+      </div>
+    </footer>
+  );
+}
